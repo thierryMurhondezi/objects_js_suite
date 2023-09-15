1429bb1b93d6177d5ed636877bc4b95366bb9403
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-15 01:12:47 +0100

    add: a new script

---
+++ script.js
@@ -0,0 +1,4 @@
+Here's an example of a simple object:
+- var myObject = { name: "John", age: 23, hobby: "hiking" };
+This object has three properties: name, age, and hobby. Each property has a different value. You can access the properties of an object using dot notation, like this:
+- myObject.name; //
